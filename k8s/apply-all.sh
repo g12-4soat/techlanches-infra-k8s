b8b7c9296971bf0a489bb2faa985c5a0910b27aa
Mateus Oliveira <47544265+aoliveiramateus@users.noreply.github.com>
2024-03-04 20:48:53 -0300

    Update apply-all.sh

--- k8s/apply-all.sh
+++ k8s/apply-all.sh
@@ -4,11 +4,11 @@
 # /> chmod +x apply-all.sh
 ls -l
 # Apply all files (including subdirectories)
-kubectl apply -f metrics/metrics.yaml
-kubectl apply -f techlanches-namespace.yaml
-kubectl apply -f /techlanches-secrets.yaml
-kubectl apply -f /hpas/techlanches-api-hpa.yaml
-kubectl apply -f /hpas/techlanches-worker-hpa.yaml
+kubectl apply -f ./k8s/metrics/metrics.yaml
+kubectl apply -f ./k8s/techlanches-namespace.yaml
+kubectl apply -f ./k8s/techlanches-secrets.yaml
+kubectl apply -f ./k8s//hpas/techlanches-api-hpa.yaml
+kubectl apply -f ./k8s/hpas/techlanches-worker-hpa.yaml
 kubectl apply -f ./k8s/deployments/techlanches-sql-deployment.yaml
 kubectl apply -f ./k8s/deployments/techlanches-api-deployment.yaml
 kubectl apply -f ./k8s/deployments/techlanches-worker-deployment.yaml
